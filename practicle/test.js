6e99ebaef1084eb620e885d3ff2c5a5df581246a
vishal <VishalChoudhary1945@gmail.com>
2022-03-09 17:46:21 +0530

    latest code

---
+++ practicle/test.js
@@ -0,0 +1,189 @@
+
+console.log("hello world")
+console.log(2+3)
+//window.alert("vishal");
+
+
+
+/*
+var  x = 5;
+var  y = 6;
+var z;
+
+
+z = x;
+x = y;
+y = z;
+
+console.log("value of x is = x")
+console.log(`value of y is = ${y}`)
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt)
+
+*/
+
+
+
+/*
+
+how to declare variable sin jaavascript
+
+
+3 ways
+
+var  -- u update
+let--- values can update
+const--cannot  updatevalues
+*/
+
+/*Arithematic operater */
+
+/* 
+var a = 5;
+var b = 6;
+
+console.log(`${a} + ${b} =${a+b}`)
+console.log(`${a} - ${b} =${a-b}`)
+console.log(`${a} * ${b} =${a*b}`)
+console.log(`${a} / ${b} =${a/b}`)
+console.log(`${a} % ${b} =${a%b}`)
+*/
+
+/*bitwise operater */
+/**
+let a = 6;
+let b = 5;
+
+console.log(`${a} & ${b} = ${a&b}`)
+console.log(`${a} | ${b} = ${a|b}`)
+console.log(`~${a} = ${~a}`) 
+console.log(`${a} & ${b} = ${a&b}`)
+console.log(`${a} & ${b} = ${a&b}`)
+*/
+
+/**relational operater */
+/*
+x = 7;
+y = 8;
+
+console.log(`${x}  < ${y} = ${x < y }`)
+console.log(`${x}  > ${y} = ${x > y }`)
+console.log(`${x} <= ${y} = ${x <= y }`)
+console.log(`${x} >= ${y} = ${x >= y }`)
+console.log(`${x} != ${y} = ${x != y }`)
+*/
+
+
+/**logical operater */
+/*
+a = 23;
+b = 24;
+
+console.log(`${a}==${a} &&  ${a}==${a} : ${a==a&&a==a}`)
+console.log(`${a}==${a} &&  ${a}==${b} : ${a==a&&a==b}`)
+console.log(`${a}==${a} &&  ${a}==${a} : ${a==a||a==a}`)
+console.log(`${a}==${a} &&  ${a}==${b} : ${a==a||a==b}`)
+console.log(`${a}==${a} &&  ${a}==${a} : ${a==a&&a==a}`)
+console.log(`${a} != ${b} : ${a != b}`)
+console.log(`${a} != ${a} : ${a != a}`)
+*/
+
+
+/**caomparison operater */
+/*
+x = 10;
+y = 10.1;
+z = 10.0;
+console.log(typeof(y));
+console.log(`${x} == ${y} : ${x == y}`)
+console.log(`${x} === ${y} : ${x === y}`)
+console.log(`${x} === ${z} : ${x === z}`)
+
+*/
+
+/*
+function fun(){
+    console.log("vishal");
+}
+
+fun();
+*/
+/**TSRN */
+
+/*
+function xyz(x,y)
+
+{
+    console.log(`${x + y}`)
+
+}
+
+x= 5;
+y= 7;
+
+xyz(x,y);
+*/
+
+/**TNRS */
+/**
+function abc()
+{
+    x = 7;
+    y = 3;
+
+    return x + y;
+}
+
+console.log(abc())
+abc();
+
+/**TSRS */
+
+function add(a,b)
+{
+    let c = a + b;
+
+    return c ;
+}
+
+let x = 9;
+let y = 8;
+
+
+console.log(`sum of ${x} and ${y} is = `,add(x,y))
+
+// add(x,y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
